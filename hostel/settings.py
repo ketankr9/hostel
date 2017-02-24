28ecac01c98963d52b8dc7e2b61e369c54ecca44
shelandraupadhyayiitbhu <shelandraupadhyayiitbhu@gmail.com>
2017-02-24 16:36:21 +0530

    set the TEMPLATE_PATH in the settings.py

--- hostel/settings.py
+++ hostel/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH=os.path.join(BASE_DIR,'templates')
+
+
 
 
 # Quick-start development settings - unsuitable for production
