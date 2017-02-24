b205210a103a11ca47f4a2c5244d7fef4446c98a
ketankr9 <ketankr9@gmail.com>
2017-02-24 11:44:41 +0530

    slight modification-U

--- myapp/views.py
+++ myapp/views.py
@@ -3,28 +3,46 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return  HttpResponse("index page of my app")
+    return render(request,'index.html',dic)
 
 def index2(request):
-    return HttpResponse("index2 page of myapp")
+    return render(request,'index2.html',dic)
 
 def contactus(request):
-    return HttpResponse("contact us page of myapp")
+    dic={}
+    return render(request,'contactus.html',dic)
 
 def grieve(request):
-    return HttpResponse("grieverience page of myapp")
+    dic={}
+    return render(request,'grieve.html',dic)
 
-def loginu(request ):
-    return HttpResponse("this is login page of myapp")
+def login(request,num):
+    dic={}
+    print "num received",num
+    #redirection here
+    # if 'cook' in request.COOKIES:
+    #     print "Cookies exists::COOKED"
+    #
+    response=render(request,'login.html',dic)
+
+    return response
 
 def discussion(request):
-    return HttpResponse("this is discussion page of my app")
+    dic={}
+    return render(request,'discussion.html',dic)
 
 def register(request):
-    return HttpResponse("this is discussion page of my app")
+    print "Registeration page"
+    if request.method=='POST':
+        data=request.POST
+        print "print here the received values eg data['usrname'],data['password'], etc"
+    return render(request,'registeration.html',dic)
 
-def studprofile(request):
-    return HttpResponse("this is student profile page of my app")
+def studprofile(request,roll):
+    print roll
+    dic={}
+    response=render(request,'index2.html',dic)
+    return response
 
 def past(request):
     return HttpResponse("this is a past page of my app")
